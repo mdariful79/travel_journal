9d7d1a26a0c342035b597da77bcedbea33465cf2
mdariful79 <mdariful057@gmail.com>
2024-12-04 11:30:50 +0600

    completed

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -7,6 +7,13 @@ export default function Entry(props) {
                     className="main-image"
                     src={props.img.src}
                     alt={props.img.alt}
+                //passing data as properties
+                // src={props.img.src}
+                // alt={props.img.alt}
+                //Passing as object we need to add .entry after the props
+                //src={props.entry.img.src}
+
+                //Spread object as props
                 />
             </div>
             <div className="info-container">
@@ -16,12 +23,12 @@ export default function Entry(props) {
                     alt="map marker icon"
                 />
                 <span className="country">{props.country}</span>
-                <a href={props.googleMapsLink}> View on Google Maps</a>
+                <a href={props.googleMapsLink} target="_blank">View on Google Maps</a>
                 <h2 className="entry-title">{props.title}</h2>
                 <p className="trip-dates">{props.dates}</p>
                 <p className="entry-text">{props.text}</p>
             </div>
 
-        </article >
+        </article>
     )
 }
---
+++ data.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "./src/assets/ahsanManzil.jpg",
+            alt: "Ahsan Manzil"
+        },
+        title: "Ahsan Manzil",
+        country: "DHAKA, BANGLADESH",
+        googleMapsLink: "https://maps.app.goo.gl/zQLVgME2UJLwAYhv5",
+        dates: "22 Nov, 2024 - 29 Nov, 2024",
+        text: "Ahsan Manzil, a striking pink palace in Dhaka, was the residence of the Nawabs during the British era. Built in the late 19th century, it now serves as a museum preserving their legacy and Bangladesh's heritage."
+    },
+    {
+        id: 2,
+        img: {
+            src: "./src/assets/sreemangal.jpg",
+            alt: "Sreemangal"
+        },
+        title: "Sreemangal",
+        country: "Sylhet, BANGLADESH",
+        googleMapsLink: "https://maps.app.goo.gl/CGwdfV5mwBEEtahe8",
+        dates: "12 June, 2024 - 21 June, 2024",
+        text: "Sreemangal is known as the tea capital of Bangladesh. Sreemangal’s tea gardens can be seen for miles and miles. The town is surrounded by more than 40 tea gardens, lakes, lofty hills, dense jungles, lemon and pineapple gardens and rubber gardens."
+        
+    },
+    {
+        id: 3,
+        img: {
+            src: "./src/assets/stMartin.webp",
+            alt: "Saint Martin's Island"
+        },
+        title: "Saint Martin's Island",
+        country: "Teknaf, BANGLADESH",
+        googleMapsLink: "https://maps.app.goo.gl/rrfeRLdn8nRJ2zap7",
+        dates: "01 Jan, 2024 - 05 Jan, 2024",
+        text: "Saint Martin's Island, located in the Bay of Bengal, is Bangladesh's only coral island, renowned for its pristine beaches and clear turquoise waters. It is a popular tourist destination for its serene beauty and unique biodiversity."
+    },
+    {
+        id: 3,
+        img: {
+            src: "./src/assets/coxsbazar.jpg",
+            alt: "Cox's Bazar Beach"
+        },
+        title: "Cox's Bazar Beach",
+        country: "cox's bazar, BANGLADESH",
+        googleMapsLink: "https://maps.app.goo.gl/F4jKMFFQw628soj3A",
+        dates: "22 Dec, 2023 - 31 Dec, 2023",
+        text: "It is the world's longest unbroken sandy sea beach, stretching over 120 kilometers. Renowned for its breathtaking sunsets and vibrant coastal culture, it is a major tourist destination."
+    },
+]
